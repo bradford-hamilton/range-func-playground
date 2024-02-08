0883c3675dd1be5f59f19ef1759d8b85cc0785b8
bradford lamson-scribner <brad.lamson@gmail.com>
2024-02-08 13:30:47 -0700

    add godoc comments

--- stack/stack.go
+++ stack/stack.go
@@ -7,14 +7,17 @@ type Stack[T any] struct {
 	items []T
 }
 
+// New returns a stack hydrated with the given items.
 func New[T any](items ...T) *Stack[T] {
 	return &Stack[T]{items: items}
 }
 
+// Push adds an item to the top of the stack.
 func (s *Stack[T]) Push(item T) {
 	s.items = append(s.items, item)
 }
 
+// Pop removes and returns the item at the top of the stack.
 func (s *Stack[T]) Pop() (T, bool) {
 	if len(s.items) == 0 {
 		var zero T
@@ -27,6 +30,7 @@ func (s *Stack[T]) Pop() (T, bool) {
 	return popped, true
 }
 
+// TopDown returns an iterator that walks down the stack from the top.
 func (s *Stack[T]) TopDown() iter.Seq[T] {
 	return func(yield func(T) bool) {
 		for i := len(s.items) - 1; i >= 0; i-- {
@@ -37,6 +41,7 @@ func (s *Stack[T]) TopDown() iter.Seq[T] {
 	}
 }
 
+// TopDown returns an iterator that walks up the stack from the bottom.
 func (s *Stack[T]) BottomUp() iter.Seq[T] {
 	return func(yield func(T) bool) {
 		for _, item := range s.items {
